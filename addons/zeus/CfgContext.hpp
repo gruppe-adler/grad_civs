d6b1ec410ee3594d245967019f410f2e11e35dea
Moritz Schmidt <fusselwurm@gmail.com>
2021-05-11 22:11:18 +0200

    wip

--- addons/zeus/CfgContext.hpp
+++ addons/zeus/CfgContext.hpp
@@ -23,5 +23,25 @@ class zen_context_menu_actions {
             condition = QUOTE(_this call FUNC(context_condition_dismissCiv));
             icon = QPATHTOF(ui\icon_zen_context_dismissCiv.paa);
         };
+        class RecruitCivs {
+            displayName = "Recruit Civs";
+            statement = QUOTE(_this call FUNC(context_statement_recruitCivs));
+            condition = QUOTE(_this call FUNC(context_condition_recruitCivs));
+            class AsResidents {
+                displayName = "as residents";
+                statement = QUOTE(_this call FUNC(context_statement_recruitCivsAsResidents));
+                condition = QUOTE(_this call FUNC(context_condition_recruitCivsAsResidents));
+            };
+            class AsVoyagers {
+                displayName = "as voyagers";
+                statement = QUOTE(_this call FUNC(context_statement_recruitCivsAsVoyagers));
+                condition = QUOTE(_this call FUNC(context_condition_recruitCivsAsVoyagers));
+            };
+            class AsPatrol {
+                displayName = "as patrol";
+                statement = QUOTE(_this call FUNC(context_statement_recruitCivsAsPatrol));
+                condition = QUOTE(_this call FUNC(context_condition_recruitCivsAsPatrol));
+            };
+        };
     };
 };
