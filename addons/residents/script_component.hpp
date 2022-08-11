bf7bdb39a329a00cd0d8e90f303194e757c5f415
Moritz Schmidt <fusselwurm@gmail.com>
2022-08-12 00:25:08 +0200

    DISABLE DEBUG MODE

--- addons/residents/script_component.hpp
+++ addons/residents/script_component.hpp
@@ -1,8 +1,8 @@
 #define COMPONENT residents
 #include "\z\grad_civs\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_MAIN
     #define DEBUG_MODE_FULL
